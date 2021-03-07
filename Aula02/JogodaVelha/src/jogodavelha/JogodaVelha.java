a90b263e29a013813498da6bd33f29cb49b99028
Fabio Lubacheski <flubacheski@gmail.com>
2021-03-07 11:50:43 -0300

    nome do commit

---
+++ Aula02/JogodaVelha/src/jogodavelha/JogodaVelha.java
@@ -0,0 +1,24 @@
+package jogodavelha;
+
+public class JogodaVelha {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char matriz[][];
+        
+        matriz = initialize();
+        
+        print(matriz);
+    }
+    public static char [][] initialize( ){
+        char tabuleiro [][] = new char [3][3];
+        
+        // preenche
+        
+        // retorna
+    }
+    
+}
